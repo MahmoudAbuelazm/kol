90427995eccf504d3f3042a3436742bbc8150f24
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-02 12:21:37 +0300

    feat: Add back button to registration screens

---
+++ lib/data/repository/get_client_data/get_client_data.dart
@@ -0,0 +1,12 @@
+// import '../../../services/network/network_manager_clone.dart';
+
+// class GetClientData{
+//   Future<ClientData> call(String clientId) async {
+//     try {
+//       final response = await NetworkManagerClone().get(loginEndPoint);
+//     return ClientData.fromJson(response);
+//     } catch (e) {
+//       rethrow;
+//     }
+//   }
+// }
